03a24a3e7fe88d5b117129729725f77ef2123df7
benslevin <64353336+benslevin@users.noreply.github.com>
2020-07-11 19:21:35 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,31 +1,48 @@
-#include "assembler.h"
+#include "main.h"
 
-int main(int argc, char* argv[])
-{
+
+reset_falgs() {
+
+}
+
+
+int main(int argc, char* argv[]){
+
+    int i;
+    FILE* fp;
     if (argc < 2) {
-        printf("No argument found, please enter a file name to process\n");
+        fprintf(stdout, "No argument found, please enter a file name to process\n");
         exit(1);
     }
     else {
-        for (int i = 1; i < argc; i++) {
-            strcpy(filename, argv[i]);
-            strcat(filename, ".as");
-            if ((fd = fopen(argv[i], "r")) == NULL)
-                fprintf(stderr, "\nAn error occured while opening the file\n");
-            exit(0)
+        for (i = 1; i < argc; i++) {
+            createFileName(argv[i]);
+            if ((fp = fopen(argv[i], "r")) == NULL) {
+                fprintf(stderr, "\nAn error occured while opening the file: %s\n", fileName);
+            }
             else {
-                if (feof(fd) == 1)/* not sure if we need to see if the file is empty here of after each char in the passOne function ep.11 1:00*/
+                if (feof(fp) == 1)/* not sure if we need to see if the file is empty here of after each char in the passOne function ep.11 1:00*/
                     fprintf(stderr, "\nThe file is empty\n");
                 else {
+                    fprintf(stdout, "*******Started working on file: %s*******", fileName);
+
+                    reset_flags();
                     passOne();/*here we can add a while loop to see if teh feof flag is on*/
-                    passTwo();
-                    fclose(fd);
+
+                    if (err == 0) {
+                        rewind(fp);
+                        passTwo();
+                    }
+                    else {
+                        fprintf(stdout, "Errors found in file: %s, stoped working on file", fileName);
+                    }
+                    
+                    fprintf(stdout, "*******Finished working on file: %s*******", fileName);
+                    fclose(fp);
                 }
             }
         }
     }
-    exit(0);
 
-
-
+    return 0;
 }
