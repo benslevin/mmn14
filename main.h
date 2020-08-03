48ff556bdc412423848dc24a8d82c249e6e2d246
alans <alanschioj@gmail.com>
2020-08-03 23:01:59 +0300

    Add more firstPass functions

--- main.h
+++ main.h
@@ -8,7 +8,21 @@
 #define MAX_LABEL 32
 #define MAX_CHAR 8
 #define DEFAULT_IC 100
+#define LINE_LENGTH 81 /* maximum chars for each line */
+#define EMPTY_ERROR 0
+
+/* Directives types */
+enum guidance { DATA, STRING, STRUCT, ENTRY, EXTERN, UNKNOWN_TYPE };
+
+/* Enum of commands ordered by their opcode */
+enum commands { MOV, CMP, ADD, SUB, LEA, CLR, NOT, INC, DEC, JMP, JSR, BNE, RED, PRN, RTS, STOP, UNKNOWN_COMMAND };
 
 char filename[MAX_INPUT];
 char line[MAX_INPUT];
 FILE* fd;
+
+
+
+enum errors {
+    SYNTAX_ERR = 1,
+};
