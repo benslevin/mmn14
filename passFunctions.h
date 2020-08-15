1680b2feea23907279a13b9db17128b2fdc44e6a
benslevin <64353336+benslevin@users.noreply.github.com>
2020-08-15 14:31:53 +0300

    Update passFunctions.h

--- passFunctions.h
+++ passFunctions.h
@@ -6,9 +6,6 @@
 #include "struct.h"
 /*This file includes all the functions from the passes*/
 
-
-
-
 /*Pass one fuctions*/
 
 void passOne(FILE* fp);
@@ -23,16 +20,16 @@ int detect_method(char* operand);
 boolean command_accept_num_operands(int type, boolean first, boolean second);
 boolean command_accept_methods(int type, int first_method, int second_method);
 unsigned int build_first_word(int type, int is_first, int is_second, int first_method, int second_method, int first_register, int second_register);
-unsigned int build_additional_word_first_pass(int operand);
 int calculate_command_num_additional_words(int is_first, int is_second, int first_method, int second_method);
 int num_words(int method);
 int command_funct(int commands);
+int find_new_type(int type);
 
 /*Pass two fuctions*/
 
 void passTwo(FILE* fp, char* filename);
 void line_pass_two(char* line);
-void creat_output_files(char* name);
+int creat_output_files(char* name);
 FILE* open_file(char* filename, int type);
 void creat_object_file(FILE* fp);
 void creat_entry_file(FILE* fp);
@@ -42,6 +39,7 @@ int handle_command_pass_two(int type, char* line);
 int encode_additional_words(char* src, char* dest, boolean is_src, boolean is_dest, int src_method, int dest_method);
 void encode_label(char* label);
 void encode_label_relative(char* label);
+void encode_num_immediate(char* operand);
 void encode_additional_word(boolean is_dest, int method, char* operand);
 
 #endif
