b5d3bd3ae4770bfa85bf4c0c6664df008443533b
benslevin <64353336+benslevin@users.noreply.github.com>
2020-08-15 18:11:06 +0300

    Update externalStruct.c

--- externalStruct.c
+++ externalStruct.c
@@ -1,9 +1,11 @@
 #include "main.h"
 #include "struct.h"
 
+/* In this file we have all functions regarding the linked list of extern labels */
+
 /* This function adds a node to the end of the list */
-extPtr add_ext(extPtr* lptr, char* name, unsigned int reference)
-{
+extPtr add_ext(extPtr* lptr, char* name, unsigned int reference) {
+
     extPtr t = *lptr;
     extPtr temp;
 
@@ -25,7 +27,6 @@ extPtr add_ext(extPtr* lptr, char* name, unsigned int reference)
         return temp;
     }
 
-
     ((*lptr)->prev)->next = temp;
     temp->next = t;
     temp->prev = t->prev;
@@ -35,8 +36,8 @@ extPtr add_ext(extPtr* lptr, char* name, unsigned int reference)
 }
 
 /* This function frees the allocated memory for the list */
-void free_ext(extPtr* lptr)
-{
+void free_ext(extPtr* lptr) {
+
     /* Frees the extern list by going over each extern node and free it*/
     extPtr temp = *lptr;
     if (temp) {
@@ -49,16 +50,4 @@ void free_ext(extPtr* lptr)
             free(temp);
         } while (reference != last_reference);
     }
-}
-
-/* This function prints the ext list */
-void print_ext(extPtr l)
-{
-    extPtr orig = l;
-    do
-    {
-        printf("\nname: %s, reference: %d - >", l->name, l->address);
-        l = l->next;
-    } while (l->address != orig->address);
-    printf("*\n");
 }
